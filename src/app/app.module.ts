1ed7bdf57302dbd351de7757a34e44497e15dadb
Sathya19-py <57808746+Sathya19-py@users.noreply.github.com>
2020-11-21 22:16:38 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatListModule} from '@angular/material/list';
+import {MatTabsModule} from '@angular/material/tabs';
+import {ProgressbarModule} from 'ngx-bootstrap/progressbar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatMenuModule,
+        MatListModule,
+        MatTabsModule,
+        ProgressbarModule,
+
+    ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA, ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
